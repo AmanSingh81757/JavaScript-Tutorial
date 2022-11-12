5cf7f1cf36d62d440ad23e3d9c073178d3bb144b
Aman Singh <amansingh81757@gmail.com>
2022-11-13 00:59:01 +0530

    added loops.js

---
+++ loops.js
@@ -0,0 +1,32 @@
+// for loops
+for(let i = 0; i < 5 ; i++){
+    console.log(i)
+}
+
+// for in loops
+let a = [1,2,3,4]
+for(let item in a){
+    console.log(a[item])
+}
+
+// for each loops
+for(let i of a){
+    console.log(i)
+}
+
+// while loops
+let num = 5
+while(num > 0){
+    console.log(num)
+    num--
+}
+
+// do-while loops
+let ans = 5
+do{
+    console.log(ans)
+}
+while(a > 6){
+    console.log(ans)
+    ans--
+}
