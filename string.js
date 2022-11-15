f728a82a0afed0f98699ae787604f18cc459fb06
Aman Singh <amansingh81757@gmail.com>
2022-11-15 20:42:56 +0530

    added string.js

---
+++ string.js
@@ -0,0 +1,9 @@
+let name = "apple"
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(2, 4))
+console.log(name.slice(2))
+console.log(name.replace("ap", "pa"))
+let friend = "banana"
+console.log(name.concat(" is a friend of ", friend, " Ok"))
